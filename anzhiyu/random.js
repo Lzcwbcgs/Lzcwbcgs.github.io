eccd6114d70115358e1a090493873c92c797862c
Lzcwbcgs <368643913@qq.com>
2025-01-18 20:01:10 +0800

    Site updated: 2025-01-18 20:01:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/hello-world/"];function toRandomPost(){
+var posts=["2025/01/18/C-游戏开发-提瓦特幸存者/","2025/01/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
