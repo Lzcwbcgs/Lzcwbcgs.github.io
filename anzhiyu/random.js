7e2c5084a6a7f17c3ebdb2b736057add0af278c8
Lzcwbcgs <368643913@qq.com>
2025-02-06 17:21:21 +0800

    Site updated: 2025-02-06 17:21:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/04/A-B高精度/","2025/01/18/C-游戏开发-提瓦特幸存者/","2025/01/18/hello-world/"];function toRandomPost(){
+var posts=["2025/02/04/A-B高精度/","2025/01/18/hello-world/","2025/01/18/C-游戏开发-提瓦特幸存者/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
