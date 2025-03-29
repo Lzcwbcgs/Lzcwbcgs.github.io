d0c3ba76a94d959b97f0c6fa2968e4713a82e9ad
Lzcwbcgs <368643913@qq.com>
2025-03-29 22:06:44 +0800

    Site updated: 2025-03-29 22:06:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/04/A-B高精度/","2025/02/18/欧拉筛/","2025/01/18/C-游戏开发-提瓦特幸存者/","2025/01/18/hello-world/","2025/03/29/线段树/"];function toRandomPost(){
+var posts=["2025/02/04/A-B高精度/","2025/01/18/C-游戏开发-提瓦特幸存者/","2025/01/18/hello-world/","2025/02/18/欧拉筛/","2025/03/29/线段树/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
